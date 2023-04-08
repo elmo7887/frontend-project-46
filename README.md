c526f6150e9d30552932726381ecbc442d7377c6
Almas Utebayev <almas7887@gmail.com>
2023-04-08 21:23:51 +0500

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/elmo7887/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/elmo7887/frontend-project-46/actions)
+
+
+# Setup
+
+> make install
+> npm link
+
+---
+
+[![asciicast](https://asciinema.org/a/J8kmrJDIau968RFg55vpxtlGh.svg)](https://asciinema.org/a/J8kmrJDIau968RFg55vpxtlGh)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
+import genDiff from '../src/index.js'
+
+const program = new Command();
 
 program
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --version,', 'output the version number') 
-  .option('-f, --format <type>', 'output format')
- 
+  .version('0.0.1', '-V, --version,', 'output the version number') 
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 program.parse();
